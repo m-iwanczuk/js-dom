b40c76fe588f226aba33053d43cbf538e8436926
m-iwanczuk <michal@e-post.pl>
2019-03-03 10:15:00 +0100

    dodano 2-finding-elments.js

--- js/1-dom.js
+++ js/1-dom.js
@@ -1,2 +1,10 @@
 console.log(document);
 console.dir(document);
+
+console.log(document.documentElement); // <html>
+console.log(document.head);
+console.log(document.body);
+
+
+console.log(window); // okno przeglądarki
+
